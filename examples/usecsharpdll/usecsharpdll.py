12c3b76d6f87c1dccac68052fef21d82966e73c0
Christoph Waibel <christophwaibel@gmx.de>
2019-10-10 11:52:30 +0200

    changed absolute to relative path of dll

--- examples/usecsharpdll/usecsharpdll.py
+++ examples/usecsharpdll/usecsharpdll.py
@@ -2,8 +2,10 @@
 example on how to load an external c# dll and use its functions.
 calculates distance between two 1D points
 """
+import Grasshopper
+path = Grasshopper.Folders.AppDataFolder
 import clr
-clr.AddReferenceToFileAndPath(r"C:\Users\chwaibel\Documents\GitHub\honey-badger\examples\usecsharpdll\SolarModel.dll") # path of your dll
+clr.AddReferenceToFileAndPath(path + "Libraries\SolarModel.dll") # need to put that dll into your Grasshopper libraries folder
 import SolarModel # your Python IDE will tell you that this module is missing, even if it isn't. Can be resolved by installing stubs for it, if any exist
 import System # also doesn't see it
 
